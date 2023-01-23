2f4c2334fab209230739a5a14ea778bbb21f4320
Emanuelle-Oliveira <manutrindadeoliver@gmail.com>
2023-01-23 20:55:57 -0300

    final project

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/** /*@type {import('next').NextConfig} */
+/*const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};*/
 
-module.exports = nextConfig
+module.exports = {
+  trailingSlash: true, // adiciona / no fim da url
+  async redirects() {
+    return [
+      {
+        source: '/perguntas', // Outras rotas
+        destination: '/faq/',
+        permanent: true,
+      },
+    ];
+  },
+};
